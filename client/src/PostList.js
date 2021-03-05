1724a061d03576ae6ece94059846c386e976d911
Michal Klauza <klauza.uk@gmail.com>
2021-03-05 13:41:21 +0000

    listing comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 // component making request to our post and list all posts ever been created
 export default () => {
@@ -26,6 +27,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
