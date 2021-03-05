4a11992938b8a4641e82d27c926496d4dcd79096
Michal Klauza <klauza.uk@gmail.com>
2021-03-05 11:54:38 +0000

    various

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PostCreate from "./PostCreate";
+
+export default () => {
+  return (
+    <div className="container">
+      <h1>The microservices 1 app</h1>
+      <PostCreate />
+    </div>
+  );
+};
