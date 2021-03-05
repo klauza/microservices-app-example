529ee0d28f5a9a7c132d7ead64fe4bf5033421e8
Michal Klauza <klauza.uk@gmail.com>
2021-03-05 13:09:56 +0000

    displaying posts in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import PostCreate from "./PostCreate";
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
   return (
     <div className="container">
       <h1>The microservices 1 app</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
---
+++ client/src/PostList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// component making request to our post and list all posts ever been created
+export default () => {
+  const [posts, setPosts] = useState({}); // posts expect an object
+
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // object.values is built-in function JS that gives back an array of all the values inside of 'posts' object
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: '30%', marginBottom: '20px' }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
