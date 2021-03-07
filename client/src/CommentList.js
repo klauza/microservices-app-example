3480c2313dd1a6d274f6c8bbc3d9e83cc8fa69cb
Michal Klauza <klauza.uk@gmail.com>
2021-03-07 14:38:31 +0000

    react rejecting syntax

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -26,7 +26,19 @@ import React from 'react';
 // comments are being pulled from query service
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+
+    if (comment.status === 'pending') {
+      content = 'This comment is awaiting moderation';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected';
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
