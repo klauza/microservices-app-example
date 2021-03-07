1c904ddd471b0d543c2b34f2cf80a5713a83241b
Michal Klauza <klauza.uk@gmail.com>
2021-03-07 11:10:51 +0000

    rebuilding react with query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
+// export default ({ postId }) => {
+//   const [comments, setComments] = useState([]);
+//   const fetchData = async () => {
+//     const res = await axios.get(
+//       `http://localhost:4001/posts/${postId}/comments`
+//     );
 
-    setComments(res.data);
-  };
+//     setComments(res.data);
+//   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
+//   const renderedComments = comments.map((comment) => {
+//     return <li key={comment.id}>{comment.content}</li>;
+//   });
+
+//   return <ul>{renderedComments}</ul>;
+// };
+
+import React from 'react';
+// comments are being pulled from query service
+export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,7 +8,8 @@ export default () => {
   const [posts, setPosts] = useState({}); // posts expect an object
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
+    // const res = await axios.get('http://localhost:4000/posts');
+    const res = await axios.get('http://localhost:4002/posts'); // query service
 
     setPosts(res.data);
   };
@@ -27,7 +28,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
