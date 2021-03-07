b5bcba9b5fa942185a57ec49937319fc16c9aab6
Michal Klauza <klauza.uk@gmail.com>
2021-03-07 10:52:28 +0000

    handlicng data in query service

--- query/index.js
+++ query/index.js
@@ -4,12 +4,46 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
-//
+// query service receives events PostCreated and CommentCreated
+
+const posts = {};
+// example of 'posts'
+// posts === {
+// 'f43f32': {
+//   id: 'f43f32',
+//   title: 'post title',
+//   comments: [{id: 'xx', content: 'comm'}, {id: 'yy', content: 'comm2'}]
+// },
+// 'f43f33': {
+//   id: 'f43f33',
+//   title: 'post title',
+//   comments: [{id: 'xx', content: 'comm'}, {id: 'yy', content: 'comm2'}]
+// }
+// }
 
 // route handlers
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
-app.post('/events', (req, res) => {});
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening 4002');
